446e7604a6a3204e2eb422da39bf5b48acd811a6
Krystina Scott <k.scott9@umiami.edu>
2015-12-14 13:51:17 -0500

    test35

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,3 @@ $(document).ready(function(){
 
 })
 
-(function($) {
-    
-  var allPanels = $('.accordion > dd').hide();
-    
-  $('.accordion > dt > a').click(function() {
-    allPanels.slideUp();
-    $(this).parent().next().slideDown();
-    return false;
-  });
-
-})(jQuery);
